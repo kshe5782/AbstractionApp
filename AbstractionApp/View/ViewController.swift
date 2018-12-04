2594ae4a97b66e8447e465574b9553cd579c8365
Shepherd <kshe5782@740s-pl-112.local>
2018-12-04 08:32:59 -0700

    Initial commit

---
+++ AbstractionApp/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbstractionApp
+//
+//  Created by Shepherd, Kayden on 12/4/18.
+//  Copyright © 2018 Ctec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
